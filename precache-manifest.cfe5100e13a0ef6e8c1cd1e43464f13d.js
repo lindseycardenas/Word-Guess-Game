d98dd4a8c8de5f968827c4b5af70595cfe5b9e68
carnitas <lindsey.cardenas@gmail.com>
2019-06-10 17:37:46 -0500

    Updates

---
+++ precache-manifest.cfe5100e13a0ef6e8c1cd1e43464f13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34abde94d35fbb9e6e37e889e31c420",
+    "url": "%20//lindseycardenas.github.io/survivor/index.html"
+  },
+  {
+    "revision": "45aacfc8db30e44146b5",
+    "url": "%20//lindseycardenas.github.io/survivor/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "%20//lindseycardenas.github.io/survivor/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "45aacfc8db30e44146b5",
+    "url": "%20//lindseycardenas.github.io/survivor/static/js/main.85ca8b44.chunk.js"
+  },
+  {
+    "revision": "15b98bd656ab344db2aa",
+    "url": "%20//lindseycardenas.github.io/survivor/static/js/runtime~main.3afb9b98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "%20//lindseycardenas.github.io/survivor/static/media/logo.5d5d9eef.svg"
+  }
+]);
